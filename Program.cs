3ea52eae391ecec69556473ef5311e19f9fa9aaf
Vulnorn <mixadrbk31@gmail.com>
2025-06-06 17:54:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    class Utilite
+    {
+        private static Random s_random = new Random();
+
+        public static int GenerateRandomNumber(int lowerLimitRangeRandom, int upperLimitRangeRandom)
+        {
+            int numberRandom = s_random.Next(lowerLimitRangeRandom, upperLimitRangeRandom);
+            return numberRandom;
+        }
+
+        public static int GetNumberInRange(int lowerLimitRangeNumbers = Int32.MinValue, int upperLimitRangeNumbers = Int32.MaxValue)
+        {
+            bool isEnterNumber = true;
+            int enterNumber = 0;
+            string userInput;
+
+            while (isEnterNumber)
+            {
+                Console.WriteLine($"Введите число.");
+
+                userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out enterNumber) == false)
+                    Console.WriteLine("Не корректный ввод.");
+                else if (VerifyForAcceptableNumber(enterNumber, lowerLimitRangeNumbers, upperLimitRangeNumbers))
+                    isEnterNumber = false;
+            }
+
+            return enterNumber;
+        }
+
+        private static bool VerifyForAcceptableNumber(int number, int lowerLimitRangeNumbers, int upperLimitRangeNumbers)
+        {
+            if (number < lowerLimitRangeNumbers)
+            {
+                Console.WriteLine($"Число вышло за нижний предел допустимого значения.");
+                return false;
+            }
+            else if (number > upperLimitRangeNumbers)
+            {
+                Console.WriteLine($"Число вышло за верхний предел допустимого значения.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
